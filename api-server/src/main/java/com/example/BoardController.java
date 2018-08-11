9fe52d6ac6efeab1fbbcd3a2478bddc12115d2ad
ctk0327 <ctk0327@naver.com>
2018-08-12 00:20:54 +0900

    Board Done

--- api-server/src/main/java/com/example/BoardController.java
+++ api-server/src/main/java/com/example/BoardController.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,4 +18,17 @@ public class BoardController {
 	@Autowired
 	BoardRepository boardrepository;
 
+	@RequestMapping(value = "/boards", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Board> findAllBoards(Sort sort) {
+		ArrayList<Board> ar = new ArrayList<>();
+		boardrepository.findAll(sort).forEach(e -> ar.add(e));
+		return ar;
+	}
+	
+	@RequestMapping(value = "/{boardid}/good", method = RequestMethod.PATCH)
+	@ResponseBody
+	public void patch(@PathVariable int boardid,@RequestParam int body) {
+		boardrepository.updateCount(boardid, body);
+	}
 }
--- api-server/src/main/java/com/example/BoardRepository.java
+++ api-server/src/main/java/com/example/BoardRepository.java
@@ -1,6 +1,15 @@
 package com.example;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BoardRepository extends PagingAndSortingRepository <Board, String> {
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE Board SET good=?2 WHERE id=?1")
+	void updateCount(int id,int good);
+
 }
+
